a6e231d54ec0775f84f7c960f4d3a5e9ccc0e95f
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2022-06-09 14:44:24 -0600

    Added splash scren, game, room, main class, data-dir, AB#200

---
+++ src/com/maledictus/main.java
@@ -0,0 +1,12 @@
+package com.maledictus;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.displaySplash();
+        game.getDirection();
+    }
+}
