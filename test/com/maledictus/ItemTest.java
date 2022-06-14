bce782f3880738d0459af22158bb7ece795b82c2
ncolby <nikko.j.colby@gmail.com>
2022-06-14 16:35:48 -0500

    Merging prep

--- test/com/maledictus/ItemTest.java
+++ test/com/maledictus/ItemTest.java
@@ -6,29 +6,32 @@ import com.maledictus.item.ItemType;
 import com.maledictus.item.key.KeyType;
 import com.maledictus.item.potion.Potion;
 import com.maledictus.item.potion.PotionType;
+import com.maledictus.item.weapon.Weapon;
 import com.maledictus.item.weapon.WeaponType;
 import org.junit.Test;
 
 public class ItemTest {
-//    @Test
-//    public void testItem_shouldCreateItem_whenConstructorIsPassedInProperArguments() {
-//        Item sword = new Item("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.SLASHING);
-//        Item sword2 = new Item("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.PIERCING);
-//        System.out.println(sword);
-//        System.out.println(sword2);
-//    }
+    @Test
+    public void testItem_shouldCreateItem_whenConstructorIsPassedInProperArguments() {
+        Item sword = ItemFactory.createItem("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.SLASHING);
+        Item sword2 = new Weapon("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.PIERCING);
+        System.out.println(sword);
+        System.out.println(sword2);
+    }
 
     @Test
     public void testItemFactoryCreateItem_shouldReturnANewItem_whenCreateItemMethodIsCalled() {
-        //Item item = ItemFactory.createItem("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.SLASHING);
-        //Item item2 = ItemFactory.createItem("Brass Key", "A key made of brass", ItemType.KEY, KeyType.DUNGEON);
-        //System.out.println(item);
-        //System.out.println(item2);
+        Item item = ItemFactory.createItem("Iron Sword", "A sharp sword made of the finest iron", ItemType.WEAPON, WeaponType.SLASHING);
+        Item item2 = ItemFactory.createItem("Brass Key", "A key made of brass", ItemType.KEY, KeyType.DUNGEON);
+        System.out.println(item);
+        System.out.println(item2);
+        item.use();
+        item2.use();
     }
 
     @Test
     public void createItemTest() {
-        Potion potion = ItemFactory.createItem("Potion of healing", "healing potion", ItemType.POTION, PotionType.HEALING);
+        Item potion = ItemFactory.createItem("Potion of healing", "healing potion", ItemType.POTION, PotionType.HEALING);
         potion.use();
     }
 }
