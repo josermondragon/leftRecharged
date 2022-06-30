6196f56fa5a14983b4d75fb4850fcc07d1336b8d
samekh_resh <C0d1ng4Lyf3>
2022-06-30 12:35:46 -0400

    AB#621 created character of a banshee for another part of the game where encounters are random.

--- src/main/java/com/maledictus/npc/npcCreate.java
+++ src/main/java/com/maledictus/npc/npcCreate.java
@@ -17,11 +17,12 @@ class npcCreate {
     public static void main(String[] args) {
         Map<Integer, String> bannyDialogue = new HashMap<>();
         bannyDialogue.put(1, "");
-        bannyDialogue.put(2, "....ahhhhhahahaahhh......");
-        bannyDialogue.put(3, "....aha......");
-        bannyDialogue.put(4, "....i've been hearing you traipsing around my kingdom......");
-        bannyDialogue.put(5, "....looting my kingdom. Killing my subjects......");
-        bannyDialogue.put(6, "I think you need a real battle");
+        bannyDialogue.put(2, "....AHHHHHHH......");
+        bannyDialogue.put(3, "....ahhhhhhh......");
+        bannyDialogue.put(4, "aiyeeeeeee");
+        bannyDialogue.put(5, "....I have the king's key if you want to get tyher......");
+        bannyDialogue.put(6, "....battle me for it......");
+
         Item healPotion = ItemFactory.createItem("health potion", "a small health potion", ItemType.POTION, PotionType.HEALING);
 
         Banshee banny = new Banshee(8, 50, "mareger"," an AMAZING banshee", true, Species.BANSHEE, bannyDialogue, healPotion);
