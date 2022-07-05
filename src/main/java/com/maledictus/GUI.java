07d376c64b7b036a91927570a2eabfe1c51d0042
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-07-05 17:01:22 -0400

    fixed issue where colors were not being displayer properly

--- src/main/java/com/maledictus/GUI.java
+++ src/main/java/com/maledictus/GUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.plaf.LayerUI;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
@@ -45,6 +47,7 @@ public class GUI extends JFrame {
         //where the game information renders
         JLabel game = new JLabel("Game: ");
         jta = new JTextPane();
+        jta.setBackground(Color.BLACK);
         jta.setEditable(false);
         abc.gridx = 0;
         abc.gridy = 0;
@@ -171,13 +174,26 @@ public class GUI extends JFrame {
         public void buttonAddText (String string){
         Document doc = jta.getDocument();
             try {
-//                jta.getDocument().insertString(0, string, null);
-                doc.insertString(doc.getLength(), string, null);
+                SimpleAttributeSet set = new SimpleAttributeSet();
+                StyleConstants.setForeground(set, Color.WHITE);
+                doc.insertString(doc.getLength(), string, set);
 
             } catch (BadLocationException exp) {
                 exp.printStackTrace();
             }
         }
+
+    public void buttonAddText (String string, Color color){
+        Document doc = jta.getDocument();
+        try {
+            SimpleAttributeSet set = new SimpleAttributeSet();
+            StyleConstants.setForeground(set, color);
+            doc.insertString(doc.getLength(), string, set);
+
+        } catch (BadLocationException exp) {
+            exp.printStackTrace();
+        }
+    }
     //Decorate main frame with Jlayer (Simulates a lamp in a dark house)
     class SpotlightLayerUI extends LayerUI<JPanel> {
         private boolean mActive;
