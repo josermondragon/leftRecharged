6cffe57d0c8a241a577a3ce6900e24e067b066f6
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-07-06 12:09:28 -0400

    fixed start page bug

--- src/main/java/com/maledictus/client/Main.java
+++ src/main/java/com/maledictus/client/Main.java
@@ -1,39 +1,11 @@
 package com.maledictus.client;
 
 
-import com.maledictus.GUI;
-import com.maledictus.Game;
 import com.maledictus.WelcomePage;
-import com.maledictus.music.GameMusic;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
-import java.text.ParseException;
-
-import static java.lang.Thread.sleep;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         WelcomePage.InitImage();
-
-        sleep(1000);
-
-
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            Game game = null;
-            try {
-                game = new Game();
-            } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                assert game != null;
-                game.initiateGame();
-            } catch (IOException | org.json.simple.parser.ParseException | ParseException | UnsupportedAudioFileException | LineUnavailableException e) {
-                e.printStackTrace();
-            }
-        });
     }
 }
